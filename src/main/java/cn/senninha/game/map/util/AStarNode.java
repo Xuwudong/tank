c1468dd3a4b7ddacb9305fe86b3278ce5a01e296
senninha <senninha@163.com>
2017-12-05 21:00:31 +0800

    A*算法，待完成

---
+++ src/main/java/cn/senninha/game/map/util/AStarNode.java
@@ -0,0 +1,62 @@
+package cn.senninha.game.map.util;
+
+import cn.senninha.game.map.Grid;
+
+public class AStarNode {
+	private AStarNode before;
+	private Grid value;
+	private AStarNode next;
+
+	public AStarNode(Grid value, double gDistance) {
+		super();
+		this.value = value;
+		this.gDistance = gDistance;
+	}
+	
+
+	public AStarNode() {
+		super();
+	}
+
+
+	public AStarNode getBefore() {
+		return before;
+	}
+
+	public void setBefore(AStarNode before) {
+		this.before = before;
+	}
+
+	public Grid getValue() {
+		return value;
+	}
+
+	public void setValue(Grid value) {
+		this.value = value;
+	}
+
+	public AStarNode getNext() {
+		return next;
+	}
+
+	public void setNext(AStarNode next) {
+		this.next = next;
+	}
+
+	public double getgDistance() {
+		return gDistance;
+	}
+
+	public void setgDistance(double gDistance) {
+		this.gDistance = gDistance;
+	}
+
+	/** 当前节点到上一个节点的距离 **/
+	private double gDistance;
+
+	@Override
+	public String toString() {
+		return "AStarNode [before=" + before + ", value=" + value + ", next=" + next + ", gDistance=" + gDistance + "]";
+	}
+
+}
