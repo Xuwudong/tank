1ace8a57ed1919e3628c25f39904391fd624cc79
senninha <senninha@163.com>
2018-01-08 09:06:32 +0800

    merge

--- src/main/java/cn/senninha/sserver/Test.java
+++ src/main/java/cn/senninha/sserver/Test.java
@@ -1,11 +1,25 @@
 package cn.senninha.sserver;
 
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import cn.senninha.sserver.lang.dispatch.HandlerFactory;
 import cn.senninha.sserver.message.HelloMessage;
 
 public class Test {
 
 	public static void main(String[] args) {
+		loggerTest();
+	}
+	
+	public static void loggerTest() {
+		Logger logger = LoggerFactory.getLogger(Test.class);
+		logger.debug("senninha" + new Date());
+	}
+	
+	public void dispatchTest() {
 		HelloMessage hm = new HelloMessage();
 		hm.setA(10);
 		hm.setB(1111);
